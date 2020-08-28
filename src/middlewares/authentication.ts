8b9a66d4e768e2fa094c5ed969c65f6a3fcc7671
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-28 17:05:57 -0300

    feat: create authentication for jwt

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../configs/env';
+
+const authenticate = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void | Response => {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    return response.status(401).json({ message: 'No token provided' });
+  }
+
+  const parts = authorization.split(' ');
+  if (parts.length !== 2) {
+    return response.status(401).json({ message: 'Invalid token' });
+  }
+
+  const [scheme, token] = parts;
+  if (!/^Bearer$/i.test(scheme)) {
+    return response.status(401).json({ message: 'Badly formatted token' });
+  }
+
+  try {
+    jwt.verify(token, JWT_SECRET);
+    return next();
+  } catch {
+    return response
+      .status(403)
+      .json({ message: 'Failed to authenticate token.' });
+  }
+};
+
+export default authenticate;
