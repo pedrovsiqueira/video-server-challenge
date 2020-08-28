15166653e281b2c7f1caaa33fcfe9ac81b51594e
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-28 17:05:17 -0300

    build: create token generator for jwt

---
+++ src/utils/tokenGenerator.ts
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+import { IUser } from '../models/User';
+import { JWT_SECRET } from '../configs/env';
+
+export const genToken = (user: IUser): string => {
+  const { _id } = user;
+  return jwt.sign({ _id }, JWT_SECRET);
+};
