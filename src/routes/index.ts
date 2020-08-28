58edee5d7b010b223d7561c0f57f2acde9438c41
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-28 20:29:02 -0300

    feat: add user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import authRoutes from './auth.routes';
 import userRoutes from './users.routes';
 
 const router = Router();
 
-router.use('/auth', userRoutes);
+router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
 
 export default router;
