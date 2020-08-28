c180b19740b9a312f337c15fa58da0fccd5e8d92
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-28 18:46:55 -0300

    feat: add /login route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import UsersController from '../controllers/userController';
 const router = Router();
 const usersController = new UsersController();
 
-router.post('/', usersController.create);
+router.post('/signup', usersController.create);
+router.post('/login', usersController.login);
 
 export default router;
