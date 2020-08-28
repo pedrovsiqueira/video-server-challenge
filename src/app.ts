302f7761c0de8a2c17617e4c64bd65cb86828176
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-28 18:43:10 -0300

    feat: import routes for users

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import './database/connection';
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/', routes);
 
 export default app;
