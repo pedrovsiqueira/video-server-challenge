8e5f4a983a2adfe9253d21cdce3c362e58225135
pedrovsiqueira <pedro.v.siqueira@gmail.com>
2020-08-28 22:48:30 -0300

    feat: add room routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
 import userRoutes from './users.routes';
+import roomsRoutes from './rooms.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/rooms', roomsRoutes);
 
 export default router;
